ab3436b8c16131315c72c686b6929c4740089577
VAIBHAV SINGH <115949686+VVISHUS@users.noreply.github.com>
2023-12-16 04:15:25 +0530

    Update Text_sentiment_prediction.py

--- Text_sentiment_prediction.py
+++ Text_sentiment_prediction.py
@@ -1,1 +1,31 @@
-T
+from textblob import TextBlob
+
+def sentiment_analysis(tweet):
+    def getSubjectivity(text):
+        return TextBlob(text).sentiment.subjectivity
+
+    def getPolarity(text):
+        return TextBlob(text).sentiment.polarity
+
+    tweet['Subjectivity'] = tweet['tweet'].apply(getSubjectivity)
+    tweet['Polarity'] = tweet['tweet'].apply(getPolarity)
+
+    def getAnalysis(score):
+        if score < 0:
+            return 'Negative'
+        elif score == 0:
+            return 'Neutral'
+        else:
+            return 'Positive'
+
+    tweet['Analysis'] = tweet['Polarity'].apply(getAnalysis)
+    return tweet
+
+import pandas as pd
+
+data = {'tweet': ["This is a positive tweet."]}
+tweet = pd.DataFrame(data)
+
+result = sentiment_analysis(tweet)
+
+print(result)
